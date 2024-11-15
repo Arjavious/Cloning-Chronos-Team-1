fc8fa2dfba197c9af0924af80e093ababdf45889
AidanChen0923 <aidan.chen@outlook.com>
2024-11-14 19:41:55 -0800

    Assigned motors to motor id for Indexer

--- Cloning-Chronos-Team-1/src/main/java/frc/robot/Constants.java
+++ Cloning-Chronos-Team-1/src/main/java/frc/robot/Constants.java
@@ -1,5 +1,9 @@
 package frc.robot;
 
-public class Constants {
+public final class Constants {
     
+    public static final class HardwarePorts {
+        public static final int indexerMotorLead = 1;
+        public static final int indexerMotorFollow = 2;
+    }
 }
--- Cloning-Chronos-Team-1/src/main/java/frc/robot/Subsystems/Indexer.java
+++ Cloning-Chronos-Team-1/src/main/java/frc/robot/Subsystems/Indexer.java
@@ -1,6 +1,9 @@
 package frc.robot.Subsystems;
 
+import com.ctre.phoenix6.controls.Follower;
+import com.ctre.phoenix6.hardware.TalonFX;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 
 public class Indexer extends SubsystemBase {
 
@@ -15,6 +18,15 @@ public class Indexer extends SubsystemBase {
         return getInstance();
     }
 
-    //fields
-    
+    //Initialize motor
+    private TalonFX LeadMotor;
+    private TalonFX followMotor;
+
+    private Follower follow = new Follower(Constants.HardwarePorts.indexerMotorFollow, false);
+
+    //Configure motors
+    public Indexer () {
+        LeadMotor = new TalonFX(Constants.HardwarePorts.indexerMotorLead);
+        followMotor = new TalonFX(Constants.HardwarePorts.indexerMotorFollow);
+    }
 }
