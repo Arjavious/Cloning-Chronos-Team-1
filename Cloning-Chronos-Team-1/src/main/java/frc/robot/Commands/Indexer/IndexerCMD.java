5ea69b6c11836be9788165c68a9332ef5f45442e
AidanChen0923 <aidan.chen@outlook.com>
2024-12-03 19:26:55 -0800

    Indexer CMD

--- Cloning-Chronos-Team-1/src/main/java/frc/robot/Commands/Indexer/IndexerCMD.java
+++ Cloning-Chronos-Team-1/src/main/java/frc/robot/Commands/Indexer/IndexerCMD.java
@@ -4,10 +4,15 @@ import edu.wpi.first.math.trajectory.ExponentialProfile.State;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Subsystems.Indexer;
 import frc.robot.Subsystems.Indexer.IndexerStates;
+import java.util.Timer;
+import java.util.TimerTask;
+import com.fasterxml.jackson.databind.node.DecimalNode;
 
 public class IndexerCMD extends Command {
     private final Indexer IndexerInstance = Indexer.getInstance();
     IndexerStates state;
+    Timer clock = new Timer();
+    
 
     //Constructor
     public IndexerCMD () {
@@ -19,7 +24,19 @@ public class IndexerCMD extends Command {
     @Override
     public void initialize() {
         IndexerInstance.setSpeed(state.getSpeed());
+        clock.schedule(null, 3000);
+    }
+
+    //finished?
+
+    @Override
+    public boolean isFinished() {
         
+    }
+
+    @Override
+    public void end(boolean cool) {
+
     }
 
 
