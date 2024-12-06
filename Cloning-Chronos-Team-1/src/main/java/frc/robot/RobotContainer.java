73b5e3746d2737fa5f2bd5bbc806f4f7398d15dc
AidanChen0923 <aidan.chen@outlook.com>
2024-12-05 19:59:06 -0800

    Robot Container
    Configurated XBOX controller and added some binds

--- Cloning-Chronos-Team-1/src/main/java/frc/robot/RobotContainer.java
+++ Cloning-Chronos-Team-1/src/main/java/frc/robot/RobotContainer.java
@@ -6,13 +6,43 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
+import frc.robot.Commands.Indexer.IndexerCMD;
+import frc.robot.Intake.IntakeCMD;
+import frc.robot.Subsystems.Indexer;
+import frc.robot.Subsystems.Intake;
+import frc.robot.Subsystems.Shooter;
+import frc.robot.Subsystems.Indexer.IndexerStates;
+import frc.robot.Subsystems.Intake.intakeStates;
 
 public class RobotContainer {
-  public RobotContainer() {
-    configureBindings();
+
+  //Imported Controller buttons/joystick
+  public final CommandXboxController driver = new CommandXboxController(0);
+
+  private final Indexer sub_Indexer = Indexer.getInstance();
+  private final Intake sub_Intake = Intake.getInstance();
+  private final Shooter sub_Shooter = Shooter.getInstance();
+
+  //Initializing Controller buttons
+  private final Trigger buttonA = driver.a();
+  private final Trigger buttonB = driver.b();
+  private final Trigger buttonX = driver.x();
+  private final Trigger buttonY = driver.y();
+
+  public CommandXboxController configBindings() {
+    return driver;
   }
 
-  private void configureBindings() {}
+  private void configureBindings() {
+    driver.a().onTrue(new IndexerCMD(IndexerStates.UP));
+    driver.b().onTrue(new IntakeCMD(intakeStates.ON));
+  }
+
+public RobotContainer() {
+    configureBindings();
+  }
 
   public Command getAutonomousCommand() {
     return Commands.print("No autonomous command configured");
